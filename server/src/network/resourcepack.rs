14e8fc31d953e2065182dfdea56ac429a450c67d
Kizunad <141109150+Kizunad@users.noreply.github.com>
2026-06-12 11:16:37 +1200

    fix(resourcepack): 同步散灵珠音效资源包哈希

--- server/src/network/resourcepack.rs
+++ server/src/network/resourcepack.rs
@@ -21,8 +21,8 @@ const DEFAULT_RESOURCE_PACK_MANIFEST: ResourcePackManifest = ResourcePackManifes
     name: "bong-full",
     version: "v1",
     file: "bong-full-v1.zip",
-    sha1: "7eaa22c753c8bbeb93e36838ff18eae973c731d4",
-    size: 72_238_086,
+    sha1: "07a7fb70d19e837677697ec27caae709f9656169",
+    size: 72_238_471,
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
