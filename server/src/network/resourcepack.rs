991aad943334c79f37bcc73d5e668db2a26e6d08
server_kizuna <serverkizuna@server.server>
2026-09-09 22:04:36 +0800

    修正资源包 CI sha1 字面量
    Model: gpt-5.6-luna

--- server/src/network/resourcepack.rs
+++ server/src/network/resourcepack.rs
@@ -21,7 +21,7 @@ const DEFAULT_RESOURCE_PACK_MANIFEST: ResourcePackManifest = ResourcePackManifes
     name: "bong-full",
     version: "v1",
     file: "bong-full-v1.zip",
-    sha1: "b25bdd305e887a67fdd5391954c146e93125e9ee",
+    sha1: "bd1e962478ddfa6dd1883c4047aa5ff8f8df17b4",
     size: 72_635_671,
 };
 
